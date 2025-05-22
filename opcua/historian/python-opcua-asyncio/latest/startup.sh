f0348e176817b79d47a79017db32154ad5aa47bb
DocDriven <sebastian.kraust@gmail.com>
2025-05-22 15:07:38 +0200

    Fix EOF error within startup script of historian

--- opcua/historian/python-opcua-asyncio/latest/startup.sh
+++ opcua/historian/python-opcua-asyncio/latest/startup.sh
@@ -16,49 +16,50 @@ DB_NAME="${DB_NAME:-/database/historian_database.sqlite3}"
 LOCKFILE="${DB_NAME:-/database/historian_database.sqlite3}.lock"
 if [ ! -f "$LOCKFILE" ]; then
 
-    # define the SQL commands for creating the tables
-    CREATE_TANK_WATER_LEVEL_TABLE="
-    CREATE TABLE IF NOT EXISTS tank_water_level (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-        pct REAL NOT NULL
-    );
-    "
+# define the SQL commands for creating the tables
+CREATE_TANK_WATER_LEVEL_TABLE="
+CREATE TABLE IF NOT EXISTS tank_water_level (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    pct REAL NOT NULL
+);
+"
 
-    CREATE_CHEMICAL_VALVE_POS_TABLE="
-    CREATE TABLE IF NOT EXISTS chemical_valve_pos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-        position INTEGER NOT NULL
-    );
-    "
+CREATE_CHEMICAL_VALVE_POS_TABLE="
+CREATE TABLE IF NOT EXISTS chemical_valve_pos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    position INTEGER NOT NULL
+);
+"
 
-    CREATE_UPPER_LIMIT_TABLE="
-    CREATE TABLE IF NOT EXISTS upper_limit (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-        pct INTEGER NOT NULL
-    );
-    "
+CREATE_UPPER_LIMIT_TABLE="
+CREATE TABLE IF NOT EXISTS upper_limit (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    pct INTEGER NOT NULL
+);
+"
 
-    CREATE_LOWER_LIMIT_TABLE="
-    CREATE TABLE IF NOT EXISTS lower_limit (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-        pct INTEGER NOT NULL
-    );
-    "
+CREATE_LOWER_LIMIT_TABLE="
+CREATE TABLE IF NOT EXISTS lower_limit (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    pct INTEGER NOT NULL
+);
+"
 
-    sqlite3 "$DB_NAME" <<EOF
-    $CREATE_TANK_WATER_LEVEL_TABLE
-    $CREATE_CHEMICAL_VALVE_POS_TABLE
-    $CREATE_UPPER_LIMIT_TABLE
-    $CREATE_LOWER_LIMIT_TABLE
-    EOF
+sqlite3 "$DB_NAME" <<EOF
+$CREATE_TANK_WATER_LEVEL_TABLE
+$CREATE_CHEMICAL_VALVE_POS_TABLE
+$CREATE_UPPER_LIMIT_TABLE
+$CREATE_LOWER_LIMIT_TABLE
+EOF
 
-    # create the file to indicate to other containers that
-    # the database is ready to use
-    touch "$LOCKFILE"
+# create the file to indicate to other containers that
+# the database is ready to use
+touch "$LOCKFILE"
+
 fi
 
 # if no ENV is set, the binary is started with defaults
